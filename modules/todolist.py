7d2c7a6af7f8536e3bfd21f9c92a473c33d34b28
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2022-02-01 08:02:16 +0100

    change place of displaying and doing comments

--- modules/todolist.py
+++ modules/todolist.py
@@ -8,7 +8,7 @@ class Todolist:
     def __init__(self, scr):
         # declaring program variables
         #---------------------------------------------------------------------------------------{{{
-        self.d, self.y={}, [0,0]
+        self.d, self.y={}, 0
         self.a_z = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789-=0!@#$%^&*()_+[]{};'\\:\"|,./<>?~\t "
         self.colors = lambda x: curses.color_pair(x)
         self.main(scr)
@@ -23,19 +23,22 @@ class Todolist:
 
             # display list of items
             # ----------------------------------------------------------------------------------{{{
+            w = (scr.getmaxyx()[1]//9)*2
             for i in range(len(self.d)):
                 item = list(self.d)[i]
                 # False         or           True
                 n = 1 if not self.d[item] else 2
                 line = f'{i+1}) {item} | {str(self.d[item]):5}'
-                self.y[1] = len(line) if self.y[1] < len(line) else self.y[1]
-                scr.addstr(i, 0, f"{line:{self.y[1]}}", self.colors(n))
-            if len(self.d) > self.y[0]:
-                line = f"{str(self.y[0]+1)+ ')' + list(self.d)[self.y[0]]}"
+                scr.addstr(i, w, f"{line:{w*2-1}}", self.colors(n))
+            # }}}----------------------------------------------------------------------------------
+            # display selected item
+            # {{{----------------------------------------------------------------------------------
+            if len(self.d) > self.y:
+                line = f"{str(self.y+1)+ ')' + list(self.d)[self.y]}"
             else:
-                line = f"{self.y[0]+1})"
-
+                line = f"{self.y+1})"
             self.clrdis_line(scr, line, 3)
+            #self.clrdis_line(scr, "nie dziala", y=self.y[0])
             # }}}----------------------------------------------------------------------------------
 
             # press arrow 
@@ -51,40 +54,46 @@ class Todolist:
             elif pressed_key == '\n':
                 if len(self.d) == 0:
                     self.insert_mode(scr)
-                elif len(self.d) != self.y[0]:
+                elif len(self.d) != self.y:
                     # self.y = 1 gdy list(self.d) = [' '] index 0
-                    k = list(self.d)[self.y[0]]
+                    k = list(self.d)[self.y]
                     self.d[k] = not self.d[k]
             # }}}----------------------------------------------------------------------------------
 
             # press a letter
             # {{{-------------------------------------------------------------------------------
             elif pressed_key in self.a_z:
-                if len(self.d) == self.y[0]:
+                if len(self.d) == self.y:
                     # pressed_key is first letter
                     self.insert_mode(scr, pressed_key)
                 else:
-                    self.insert_mode(scr, list(self.d)[self.y[0]])
+                    self.insert_mode(scr, list(self.d)[self.y]+pressed_key)
 
             # }}}----------------------------------------------------------------------------------
             
             # display options
-            self.clrdis_line(scr, "nie dziala", y=self.y[0]+1)
 
-            scr.addstr(scr.getmaxyx()[0]-1, 16, f"{self.y[0]}")
+            scr.addstr(scr.getmaxyx()[0]-1, 16, f"{self.y}")
 
 
+    def clrdis_line(self, scr, line, n=1, y=0):
+        w = (scr.getmaxyx()[1]//9)*2
+        y = self.y if y==0 else 0
+        scr.addstr(y, w, f"{'':{w}}", self.colors(4))
+        scr.addstr(y, w, line, self.colors(n))
 
     def insert_mode(self, scr, item=''):
         "docstring of method enter_item"
         
         scr.addstr(scr.getmaxyx()[0]-1, 0, "INSERT")
         while True:
-            self.y[1] = len(item) if self.y[1] < len(item) else self.y[1]
-            self.clrdis_line(scr, f'{self.y[0]+1}) {item}')
+            self.clrdis_line(scr, f'{self.y+1}) {item}')
             key = scr.getkey()
+            # press backspace
+            # {{{----------------------------------------------------------------------------------
             if key in ("KEY_BACKSPACE", '\b', '\x7f'):
                 item = item[:-1]
+            # }}}----------------------------------------------------------------------------------
             # press enter
             #{{{-----------------------------------------------------------------------------------
             elif key in '\n':
@@ -92,31 +101,30 @@ class Todolist:
                     scr.addstr(len(self.d)+1, 0, f'{repr(item)} juz istnieje')
                 else:
                     scr.addstr(scr.getmaxyx()[0]-1, 0, "SELECT")
-                    self.y[0] += 1
+                    self.y += 1
                 break
             #}}}-----------------------------------------------------------------------------------
+            # press letter
+            # {{{----------------------------------------------------------------------------------
             elif key in self.a_z:
                 item += key
+            # }}}----------------------------------------------------------------------------------
 
-        if len(self.d) == self.y[0]-1:
+        if len(self.d) == self.y-1:
             self.d[item] = False
         else:
-            olditem = list(self.d)[self.y[0]-1]
+            olditem = list(self.d)[self.y-1]
             self.d[item] = self.d.pop(olditem)
 
-    def clrdis_line(self, scr, line, n=1, y=0):
-        y = self.y[0] if y==0 else 0
-        scr.addstr(y, 0, ' '*self.y[1], self.colors(4))
-        scr.addstr(y, 0, line, self.colors(n))
 
     def press_arrow(self, key):
         "docstring press arrow"
         if key == 'KEY_UP':
-            if self.y[0] > 0:
-                self.y[0] -= 1
+            if self.y > 0:
+                self.y -= 1
         if key == 'KEY_DOWN':
-            if self.y[0] < len(self.d):
-                self.y[0] += 1
+            if self.y < len(self.d):
+                self.y += 1
 
     def program_colors(self, scr):
         curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
