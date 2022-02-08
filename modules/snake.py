53ef5f04bd619139cf7d1cdc9843b2d2aeea054a
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2022-02-08 20:53:06 +0100

    add function in snake

--- modules/snake.py
+++ modules/snake.py
@@ -1,41 +1,56 @@
 from curses import wrapper
 import curses
+import random
 
 class Snake:
     def __init__(self, scr):
         self.xy = [0, 0]
         self.direction = 'RIGHT'
+        self.fruit = [0,0]
+        self.snake = []
+
         while True:
             scr.addstr("press any key to start")
             scr.getkey()
+            scr.addstr(0,0,"                      ")
             break
         self.main(scr)
 
     def main(self, scr):
+        hw = scr.getmaxyx()
+        self.make_fruit(scr,hw)
         while True:
-            h,w = scr.getmaxyx()
             key = scr.getkey()
-            if key == 'KEY_UP':
-                if self.xy[1]-1 < 0:
-                    self.xy[1] = h-1
-                else:
-                    self.xy[1] -= 1
-            if key == 'KEY_DOWN':
-                if self.xy[1]+1 >= h-1:
-                    self.xy[1] = 0
-                self.xy[1] += 1
-            if key == 'KEY_RIGHT':
-                if self.xy[0]+1 >= w-1:
-                    self.xy[0] = 0
-                self.xy[0] += 1
-            if key == 'KEY_LEFT':
-                if self.xy[0]-1 <0:
-                    self.xy[0] = w-1
-                self.xy[0] -= 1
-            
+            hw = scr.getmaxyx()
+            self.press_an_arrow(scr,key,hw)
             scr.addstr(self.xy[1], self.xy[0], ' ')
 
+    def press_an_arrow(self,scr, key, hw):
+        h,w = hw
+        if key == 'KEY_UP':
+            if self.xy[1]-1 < 0:
+                self.xy[1] = h-1
+            self.xy[1] -= 1
+        if key == 'KEY_DOWN':
+            if self.xy[1]+1 >= h-1:
+                self.xy[1] = 0
+            self.xy[1] += 1
+        if key == 'KEY_RIGHT':
+            if self.xy[0]+1 >= w-1:
+                self.xy[0] = 0
+            self.xy[0] += 1
+        if key == 'KEY_LEFT':
+            if self.xy[0]-1 <0:
+                self.xy[0] = w-1
+            self.xy[0] -= 1
 
+    def make_fruit(self, scr, hw):
+        h,w = [int(_*random.random()) for _ in hw]
+        self.fruit = [h,w]
+        scr.addstr(h, w, '_')
+
+    def eat_fruit(self):
+        self.snake.append((self.xy[0], self.xy[1]))
             
 
 
--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,12 @@
 import curses
-import curses.ascii
+import curses.textpad
 
-def main(scr):
-    curses.napms(2000)
-    #key = scr.getstr().decode('UTF-8')
-    key = scr.getch()
-    #scr.addstr(repr(key))
-    scr.addstr(repr(key))
-    curses.endwin()
-    print(repr(key))
-    print(chr(key))
+def main(stdscr):
+    win = curses.newwin(5, 60, 5, 10)
+
+    tb = curses.textpad.Textbox(win, insert_mode=True)
+    text = tb.edit()
+    win.addstr(0, 0, text.encode('utf-8'))
+    win.getch()
+
 curses.wrapper(main)
