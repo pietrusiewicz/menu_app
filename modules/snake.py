e13f6cbd409312bae7ca457d5e1e7fd05cc6a2c8
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2022-02-09 18:02:25 +0100

    eating fruits by snake

--- modules/snake.py
+++ modules/snake.py
@@ -20,38 +20,48 @@ class Snake:
         hw = scr.getmaxyx()
         self.make_fruit(scr,hw)
         while True:
+            #self.make_full_snake()
             key = scr.getkey()
             hw = scr.getmaxyx()
             self.press_an_arrow(scr,key,hw)
-            scr.addstr(self.xy[1], self.xy[0], ' ')
+            scr.addstr(self.xy[1], self.xy[0], '')
 
     def press_an_arrow(self,scr, key, hw):
         h,w = hw
         if key == 'KEY_UP':
             if self.xy[1]-1 < 0:
                 self.xy[1] = h-1
-            self.xy[1] -= 1
+            else:
+                self.xy[1] -= 1
         if key == 'KEY_DOWN':
             if self.xy[1]+1 >= h-1:
                 self.xy[1] = 0
-            self.xy[1] += 1
+            else:
+                self.xy[1] += 1
         if key == 'KEY_RIGHT':
             if self.xy[0]+1 >= w-1:
                 self.xy[0] = 0
-            self.xy[0] += 1
+            else:
+                self.xy[0] += 1
         if key == 'KEY_LEFT':
             if self.xy[0]-1 <0:
                 self.xy[0] = w-1
-            self.xy[0] -= 1
+            else:
+                self.xy[0] -= 1
+        if self.xy == self.fruit:
+            self.make_fruit(scr, hw)
+
+        scr.addstr(h-1,0, str(self.xy))
+        scr.addstr(h-1,8, str(self.fruit))
 
     def make_fruit(self, scr, hw):
+        scr.addstr(self.xy[1], self.xy[0], ' ')
+        self.snake.append(self.xy)
         h,w = [int(_*random.random()) for _ in hw]
-        self.fruit = [h,w]
-        scr.addstr(h, w, '_')
+        self.fruit = [w,h]
+        scr.addstr(h, w, '0')
 
-    def eat_fruit(self):
-        self.snake.append((self.xy[0], self.xy[1]))
-            
-
+    #def make_full_snake(self):
+        
 
 wrapper(Snake)
