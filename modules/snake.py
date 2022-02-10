fbebcb0ce0db6c0b9981e7700258e3e59e3a99f7
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2022-02-10 15:38:15 +0100

    make basic snake in curses

--- modules/snake.py
+++ modules/snake.py
@@ -1,89 +1,71 @@
-from curses import wrapper
 import curses
 import random
 
 class Snake:
     def __init__(self, scr):
-        self.xy = [0, 0]
-        self.direction = 'RIGHT'
-        self.fruit = [0,0]
-        self.snake = []
-
-        while True:
-            scr.addstr("press any key to start")
-            scr.getkey()
-            scr.addstr(0,0,"                      ")
-            break
+        h,w = [_//2 for _ in scr.getmaxyx()]
+        self.snake,self.game = [[w-2,h],[w-1,h],[w,h]], True
+        self.xy, self.fruit = [w, h],[0,0]
         self.main(scr)
 
     def main(self, scr):
-        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_YELLOW)
-        curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_GREEN)
-        hw = scr.getmaxyx()
-        self.make_fruit(scr,hw)
-        while True:
-            self.clear_board(scr)
-            self.make_full_snake(scr)
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_GREEN)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_YELLOW)
+        self.create_fruit(scr)
+        moves=0
+        while self.game:
+            moves += 1
             key = scr.getkey()
-            hw = scr.getmaxyx()
-            self.press_an_arrow(scr,key,hw)
+            self.press_key(scr, key)
+            if self.xy in self.snake and moves > 3:
+                self.game=False
+            scr.addstr(self.xy[1], self.xy[0], '')
+            if self.xy == self.fruit:
+                self.snake.append([int(self.xy[0]), int(self.xy[1])])
+                self.create_fruit(scr)
+            self.clear_and_display_board(scr)
 
-    def make_full_snake(self,scr):
-        for x,y in self.snake:
-            scr.addstr(y, x, ' ', curses.color_pair(1))
-        scr.addstr(self.fruit[1], self.fruit[0], '0', curses.color_pair(2))
-        scr.addstr(self.xy[1], self.xy[0], '',curses.color_pair(1))
-            #scr.addstr(10,0, f"{x} {y}")
-
-    def clear_board(self, scr):
-        h, w = scr.getmaxyx()
-        for y in range(h-1):
-            for x in range(w-1):
-                scr.addstr(y, x, ' ')
-
-    # eat a fruit
-    def eat_fruit(self):
-        self.snake.append(self.xy)
-
-    # make a fruit
-    def make_fruit(self, scr, hw): #{{{
-        #scr.addstr(self.xy[1], self.xy[0], ' ', curses.color_pair(1))
-        #self.snake.append(self.xy)
-        h,w = [int(_*random.random()) for _ in hw]
-        self.fruit = [w,h]
-        scr.addstr(h, w, '0', curses.color_pair(2)) #}}}
-
-    # press an arrow
-    def press_an_arrow(self,scr, key, hw): # {{{
-        h,w = hw
+    def press_key(self, scr, key):
+        h,w = scr.getmaxyx()
         if key == 'KEY_UP':
-            if self.xy[1]-1 < 0:
-                self.xy[1] = h-1
-            else:
+            if self.xy[1] > 0:
                 self.xy[1] -= 1
+            else:
+                self.xy[1] = h-2
         if key == 'KEY_DOWN':
-            if self.xy[1]+1 >= h:
-                self.xy[1] = 0
-            else:
+            if self.xy[1] < h-2:
                 self.xy[1] += 1
-        if key == 'KEY_RIGHT':
-            if self.xy[0]+1 >= w:
-                self.xy[0] = 0
             else:
-                self.xy[0] += 1
+                self.xy[1] = 0
         if key == 'KEY_LEFT':
-            if self.xy[0]-1 <0:
-                self.xy[0] = w-1
-            else:
+            if self.xy[0] > 0:
                 self.xy[0] -= 1
+            else:
+                self.xy[0] = w-1
+        if key == 'KEY_RIGHT':
+            if self.xy[0] < w-1:
+                self.xy[0] += 1
+            else:
+                self.xy[0] = 0
         if key == '\n':
-            scr.addstr(h-1,0, str(self.xy))
-            scr.addstr(h-1,8, str(self.fruit))
+            self.game = False
 
-        if self.xy == self.fruit:
-            self.eat_fruit()
-            self.make_fruit(scr, hw)
+    def create_fruit(self, scr):
+        h,w = [int((_-1)*random.random()) for _ in scr.getmaxyx()]
+        self.fruit = [w,h]
+        scr.addstr(h, w, "0", curses.color_pair(2))
 
-        # }}}
+    def clear_and_display_board(self, scr):
+        h,w = scr.getmaxyx()
+        self.snake = self.snake[1:];self.snake.append([self.xy[0], self.xy[1]])
+        for y in range(h-1):
+            for x in range(w):
+                scr.addstr(y, x, " ")
+        scr.addstr(self.fruit[1], self.fruit[0], '0', curses.color_pair(2))
+        for x,y in self.snake:
+            scr.addstr(y,x,'=', curses.color_pair(1))
+        scr.addstr(h-1,0, f"{len(self.snake)}, {self.fruit}", curses.color_pair(2))
+        scr.addstr(self.xy[1], self.xy[0], '', curses.color_pair(1))
 
-wrapper(Snake)
+        
+curses.wrapper(Snake)
--- sandbox.py
+++ sandbox.py
@@ -1,16 +1,30 @@
 import curses
 import curses.textpad
+import random
 
 class Snake:
     def __init__(self, scr):
-        self.snake, self.xy,self.game = [], [0,0], True
+        h,w = [_//2 for _ in scr.getmaxyx()]
+        self.snake,self.game = [[w-2,h],[w-1,h],[w,h]], True
+        self.xy, self.fruit = [w, h],[0,0]
         self.main(scr)
 
     def main(self, scr):
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_GREEN)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_YELLOW)
+        self.create_fruit(scr)
+        moves=0
         while self.game:
+            moves += 1
             key = scr.getkey()
             self.press_key(scr, key)
+            if self.xy in self.snake and moves > 3:
+                break
             scr.addstr(self.xy[1], self.xy[0], '')
+            if self.xy == self.fruit:
+                self.snake.append([int(self.xy[0]), int(self.xy[1])])
+                self.create_fruit(scr)
+            self.clear_and_display_board(scr)
 
     def press_key(self, scr, key):
         h,w = scr.getmaxyx()
@@ -18,9 +32,9 @@ class Snake:
             if self.xy[1] > 0:
                 self.xy[1] -= 1
             else:
-                self.xy[1] = h-1
+                self.xy[1] = h-2
         if key == 'KEY_DOWN':
-            if self.xy[1] < h-1:
+            if self.xy[1] < h-2:
                 self.xy[1] += 1
             else:
                 self.xy[1] = 0
@@ -36,5 +50,23 @@ class Snake:
                 self.xy[0] = 0
         if key == '\n':
             self.game = False
+
+    def create_fruit(self, scr):
+        h,w = [int((_-1)*random.random()) for _ in scr.getmaxyx()]
+        self.fruit = [w,h]
+        scr.addstr(h, w, "0", curses.color_pair(2))
+
+    def clear_and_display_board(self, scr):
+        h,w = scr.getmaxyx()
+        self.snake = self.snake[1:];self.snake.append([self.xy[0], self.xy[1]])
+        for y in range(h-1):
+            for x in range(w):
+                scr.addstr(y, x, " ")
+        scr.addstr(self.fruit[1], self.fruit[0], '0', curses.color_pair(2))
+        for x,y in self.snake:
+            scr.addstr(y,x,'=', curses.color_pair(1))
+        scr.addstr(h-1,0, f"{len(self.snake)}, {self.fruit}", curses.color_pair(2))
+        scr.addstr(self.xy[1], self.xy[0], '', curses.color_pair(1))
+
         
 curses.wrapper(Snake)
