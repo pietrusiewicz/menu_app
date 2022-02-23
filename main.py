f0a5659b2f993b63614e311c6716ff0a2532abd9
Kacper Pietrusiewicz <p4zdaninho.cr@gmail.com>
2022-02-23 17:03:37 +0100

    moze teraz push sie uda...

--- main.py
+++ main.py
@@ -64,7 +64,9 @@ class Menu:
         self.d.table_name='texts'
         just_created = self.d.check_table()
         r = Reader()
+        s = Select()
         if just_created:
+            s.get_filename(scr)
             r.select_file_to_read(scr)
         t = self.d.select("WHERE last_open=1")[0]
         r.main(scr, t)
