4f9c5f52df1ff43034de08c8ab426dcbc302ad05
pazdaninho <p4zdaninho.cr@gmail.com>
2022-06-07 08:50:55 +0200

    make read function in crud

--- crud/database.py
+++ crud/database.py
@@ -4,8 +4,12 @@ class Database:
     def __init__(self):
         self.con = sqlite3.connect('db.db')
         self.cur = self.con.cursor()
+        self.table_name = ''
 
     def create(self, cols):
         "cols is string what defines columns real or text"
         self.cur.execute(f'CREATE TABLE {self.table} ({cols})')
 
+    def read(self, rows='*', where=1):
+        "select sql' command"
+        self.cur.execute(f'SELECT {rows} FROM {self.table_name} WHERE {where}}')
