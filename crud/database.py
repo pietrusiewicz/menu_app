d7258db87f83c638e397212bfecd0444b40aa0a3
pazdaninho <p4zdaninho.cr@gmail.com>
2022-06-09 07:37:00 +0200

    add update function

--- crud/database.py
+++ crud/database.py
@@ -11,5 +11,11 @@ class Database:
         self.cur.execute(f'CREATE TABLE {self.table} ({cols})')
 
     def read(self, rows='*', where=1):
-        "select sql' command"
+        "SELECT sql' command"
         self.cur.execute(f'SELECT {rows} FROM {self.table_name} WHERE {where}}')
+
+    def update(self, sets='*', where=1):
+        "UPDATE sql' command
+        example: self.update(sets='col1=val1, col2=val2', where='val1 > 0')
+        "
+        self.cur.execute(f'UPDATE {self.table_name} SET {sets} WHERE {where}}')
