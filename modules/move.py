2d4e0103dcac1431576fd4602d4cbe80c32db5f5
Kacper Pietrusiewicz <63927762+pietrusiewicz@users.noreply.github.com>
2024-02-09 12:34:54 +0100

    repair tile list
    after enter in todolist you canadd new item, before you couldn't

--- modules/move.py
+++ modules/move.py
@@ -7,27 +7,31 @@ class Move:
         self.x, self.y = 0,0
 
     def press_key(self, scr, cnds=[1,1,1,1], strictness=0):
+        """
+        cnds - there are CONDITIONS for possibility pressing a key
+        strictness - if strictness == 1, then returning value in range [0,3]
+        """
         k = scr.getkey()
         if k in ['KEY_RIGHT', 'KEY_LEFT', 'KEY_UP', 'KEY_DOWN']:
             if k == 'KEY_UP' and cnds[0]:
-                self.y = self.y-1 if cnds[0] == 1 else int(cnds[0])
                 if strictness:
                     return 0
+                else: self.y = self.y-1 if cnds[0] == 1 else int(cnds[0])
 
             if k == 'KEY_DOWN' and cnds[1]:
-                self.y = self.y+1 if cnds[1] == 1 else int(cnds[1])
                 if strictness:
                     return 1
+                else: self.y = self.y+1 if cnds[1] == 1 else int(cnds[1])
 
             if k == 'KEY_LEFT' and cnds[2]:
-                self.x = self.x-1 if cnds[2] == 1 else int(cnds[2])
                 if strictness:
                     return 2
-
+                else: self.x = self.x-1 if cnds[2] == 1 else int(cnds[2])
+            
             if k == 'KEY_RIGHT' and cnds[3]:
-                self.x = self.x+1 if cnds[3] == 1 else int(cnds[3])
                 if strictness:
                     return 3
+                else: self.x = self.x+1 if cnds[3] == 1 else int(cnds[3])
             return False
         else:
             return k
@@ -72,7 +76,7 @@ class Move:
 
 
     # move in tile
-    def tile_app(self, scr, win, d=['nic', 'takiego']):
+    def tile_app(self, scr, win, d={'nic': 'takiego'}):
         """
         second level in app
         display_tiles -> tile_app
@@ -81,19 +85,19 @@ class Move:
         while True:
 
             # display todolist in tile
-            scr.refresh()
+            #scr.refresh()
             self.fill_color(win, 2)
             for i, line in enumerate(d):
                 c = 6 if d[line] else 5
                 win.addstr(i, 0, f"{line:{w}}", curses.color_pair(c))
                 if self.y == i:
                     win.addstr(i, 0, f"{line:{w}}", curses.color_pair(c) + curses.A_UNDERLINE)
+            
 
+            win.addstr(len(d), 0, f'{"+":{w}}', curses.color_pair( 4 if len(d) == self.y else 2 ) )
 
-            win.addstr(len(d), 0, f'{"+":{w}}', curses.color_pair(4 if len(d) == self.y else 2))
+          
             win.refresh()
-
-
             # press a key
             k = self.press_key(scr, [self.y>0, self.y<len(d), 0,0])
 
@@ -104,20 +108,21 @@ class Move:
             else:
                 # delete item
                 if k in ("KEY_BACKSPACE", '\b', '\x7f'):
-                    item = list(d)[self.y]
-                    win.addstr(self.y,0, f"Are you sure to delete {item}? y/n")
-                    while k := self.press_key(win, [0,0,0,0]).lower():
-                        if k == 'y':
-                            d.pop(item)
-                            break
-                        elif k == 'n':
-                            break
+                    if self.y < len(d):
+                        item = list(d)[self.y]
+                        win.addstr(self.y,0, f"Are you sure to delete {item}? y/n")
+                        while k := self.press_key(win, [0,0,0,0]).lower():
+                            if k == 'y':
+                                d.pop(item)
+                                break
+                            elif k == 'n':
+                                break
 
-                # exit tile
+                # exit tile - ESC key
                 elif ord(k) == 27:
                     break
 
-                # press letter       A - z
+                # press letter        A - z
                 elif ord(k) in range(32, 127):
                     # edit item
                     if self.y < len(d):
@@ -135,13 +140,13 @@ class Move:
                     if self.y < len(d):
                         d[list(d)[self.y]] = not d[list(d)[self.y]]
                     else:
-                        key = self.edit_line(win, k)
+                        key = self.edit_line(win)
                         d[key] = False
         return d
 
     
     # edit line in tile
-    def edit_line(self, win, item):
+    def edit_line(self, win, item=''):
         """
         third level in app
         display_tiles -> tile_app -> edit_line
@@ -157,6 +162,7 @@ class Move:
                 # exit insert mode 
                 elif ord(key) == 10:
                     break
+                  
                 # press letter
                 elif key in self.a_z:
                     item += key
